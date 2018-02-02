d89ae70d4d3223d8668cfdb29abf89e7e1397135
James Nylen <jnylen@gmail.com>
2018-02-02 15:37:23 -0600

    Remove unused import

--- client/my-sites/people/people-list-item/index.jsx
+++ client/my-sites/people/people-list-item/index.jsx
@@ -3,13 +3,11 @@
 /**
  * External dependencies
  */
-
 import React from 'react';
 import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Gridicon from 'gridicons';
 
 /**
  * Internal dependencies
